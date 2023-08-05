70f2087a4bdcb271f004f4aaba30f59078a53ce3
David88c <dudyco@gmail.com>
2023-08-05 10:47:42 +0300

    push2

---
+++ Input_print_function/Tasks/Homework_03.py
@@ -0,0 +1,3 @@
+#We provide an input of a float number , and we need to round them to the 2nd and 3rd decimal places
+#respectively. The obtained results should be displayed separated by a space on a single line.
+
