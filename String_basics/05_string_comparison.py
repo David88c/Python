209f6608733bc7e3447492b771022143d758edf4
David88c <dudyco@gmail.com>
2023-08-07 20:34:35 +0300

    070823

--- String_basics/05_string_comparison.py
+++ String_basics/05_string_comparison.py
@@ -5,4 +5,12 @@ print(ord("a"))
 print(ord("A"))
 
 print('a' > 'cde')
-print('a' > 'cde')
+print('abc' > 'bc')
+
+print('a' > 'A')
+
+# print(ord("AA"))  # TypeError: ord() expected a character, but string of length 2 found
+
+print('abcde' > 'abcd')
+
+print(chr(97))
