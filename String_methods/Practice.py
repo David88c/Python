3fd2733924f2e1f8c5d67530b858b6454f012c27
David88c <dudyco@gmail.com>
2023-08-17 21:56:13 +0300

    170823

---
+++ String_methods/Practice.py
@@ -0,0 +1,7 @@
+#text = input()
+#print(text.upper())
+
+#------------------
+#asdertgfds
+text2 = input()
+print(text2.upper()[-3:-9]")
---
+++ String_methods/title_method.py
@@ -0,0 +1,7 @@
+message = "HellO WorlD"
+
+print(message.lower())
+print(message.upper())
+print(message.capitalize())
+print(message.title())
+print(message.translate())
