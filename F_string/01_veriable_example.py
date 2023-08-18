1490e1fadddc7ebb3e92dc7b27a715a1b87fb9b3
David88c <dudyco@gmail.com>
2023-08-18 08:02:13 +0300

    1808

--- F_string/01_veriable_example.py
+++ F_string/01_veriable_example.py
@@ -3,3 +3,11 @@ action = "study"
 
 print("I " + action + " on " + platform_name + " platform.")
 print(f"I {action} on {platform_name} platform.")
+
+#-----------------------------------------------------------
+
+platform_name = input("Which platform do you use? ")
+action = input("What do you do? (study / teach)")
+
+print("I " + action + " on " + platform_name + " platform.")
+print(f"I {action} on {platform_name} platform.")
