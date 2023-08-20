8f6bbdc6d6797a46104ae0469c20a27dcfb34cbb
David88c <dudyco@gmail.com>
2023-08-20 18:21:25 +0300

    200823

--- String_methods/HomeWork.py
+++ String_methods/HomeWork.py
@@ -1,6 +1,12 @@
-str1 = input("Enter a message: ")
-print(str1.lower())
-print(str1.upper())
-print(str1.capitalize())
-print(str1.title())
-print(f'Words: {sorted(iter(key=None)str1.split())}')
+def getlastchar(s):
+	return s[len(s):1]
+
+str1 = str("This is a test message!")
+#str1 = input("Enter a message: ")
+print(f"Lowercase:  {str1.lower()}")
+print(f"Uppercase: {str1.upper()}")
+print(f"Capitalized: {str1.capitalize()}")
+print(f"Title Case: {str1.title()}")
+print(f'Words: {sorted(str1.split(), key=getlastchar)}')
+print(f"Alphabetic First Word: {str1.split(' ',1)[0]}")
+print(f"Alphabetic Last Word: {str1.split(' ',-1)[-1]}")
