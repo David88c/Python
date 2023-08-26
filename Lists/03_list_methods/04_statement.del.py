30a01bd664c26afb27ac08b46d9949f0db48f513
David88c <dudyco@gmail.com>
2023-08-26 12:58:36 +0300

    555

--- Lists/03_list_methods/04_statement.del.py
+++ Lists/03_list_methods/04_statement.del.py
@@ -1,2 +1,11 @@
-names = ['Erez', 'Zion', 'Siri']
-print(f"{names = }")
+my_list = [0, 10, 20, 30, 40, 50]
+print(f"{my_list = }")
+
+del my_list[0:2]
+print(f"{my_list = }")
+
+del my_list[-1]
+print(f"{my_list = }")
+
+del my_list[::]
+print(f"{my_list = }")
