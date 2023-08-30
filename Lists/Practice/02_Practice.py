f43187f12afaf8637becb83c96fca8c48e54b557
David88c <dudyco@gmail.com>
2023-08-30 12:42:31 +0300

    300823-1

--- Lists/Practice/02_Practice.py
+++ Lists/Practice/02_Practice.py
@@ -2,15 +2,8 @@
 #True if the value 777 is a part
 #of the given list, otherwise output False
 
-# Function to check if 777 is in the list
-def has_777(input_list):
-    return 777 in input_list
+num= list(map(int, input("Enter numbers: : ").split()))
 
-# Input list of integers
-input_list = [1, 2, 3, 4, 5, 777, 8, 9]
+print(777 in num)
 
-# Check if 777 is in the list
-result = has_777(input_list)
 
-# Output the result
-print(result)
---
+++ Tuples/01_basics/08_count.py
@@ -0,0 +1,4 @@
+this_tuple=1,2,3,4,5,6,7,8,1
+count_of_elements=this_tuple.count(99)
+
+print(count_of_elements)
