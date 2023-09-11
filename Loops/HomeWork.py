e321369612e265a63569a502ab431ba3d4658398
David88c <dudyco@gmail.com>
2023-09-11 20:33:02 +0300

    110923

---
+++ Loops/HomeWork.py
@@ -0,0 +1,8 @@
+upper_number = int(input(range("Enter a number: "))1, 30)
+for i in range(upper_number + 1):
+    if i % 4 == 0 and i % 7 == 0:
+        print("Israel Forever")
+    elif i % 4 == 0:
+        print("Israel")
+    elif i % 5 == 0:
+        print("Forever")    
--- Loops/Practice/13_Practice.py
+++ Loops/Practice/13_Practice.py
@@ -1,6 +1,5 @@
 i = 1
 while i <= 100:
-    print(i)
-    i = i + 2
-    
-    
+    if i % 2 != 0:
+        print(i)
+    i += 1
