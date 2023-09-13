24ba68771aaed5d2ce025e66696d49c254e9247d
David88c <dudyco@gmail.com>
2023-09-13 21:09:24 +0300

    130923

--- Enumerate/02_enumerate_for_loop.py
+++ Enumerate/02_enumerate_for_loop.py
@@ -1,4 +1,4 @@
-my_list = [10,20,30,40,50]
+my_list = [10, 20, 30, 40, 50]
 
 for item in my_list:
     print(item)
@@ -7,4 +7,4 @@ for item in enumerate(my_list):
     print(item)
 
 for index, item in enumerate(my_list):
-    print(f"{index}. {item}")
+    print(f"{index+1}. {item} - Completed")
--- Loops/HomeWork.py
+++ Loops/HomeWork.py
@@ -1,8 +1,16 @@
-upper_number = int(input(range("Enter a number: "))1, 30)
-for i in range(upper_number + 1):
+upper_number = int(input("Enter a number: "))
+
+while upper_number < 30:
+    print("The number is lower than 30: ")
+    upper_number = int(input("Try again: "))
+    
+for i in range(1, upper_number + 1):
     if i % 4 == 0 and i % 7 == 0:
-        print("Israel Forever")
+            print("Israel Forever") 
     elif i % 4 == 0:
-        print("Israel")
+           print("Israel")
     elif i % 5 == 0:
-        print("Forever")    
+           print("Forever")
+    else:
+        print(i)           
+                              
