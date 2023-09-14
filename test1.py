442d74be55865c895306341a9cf56d815a906e4f
David88c <dudyco@gmail.com>
2023-09-14 21:41:11 +0300

    140923

--- test1.py
+++ test1.py
@@ -1,3 +1,15 @@
-num1 = int(input("Enter"))
-num2 = int(input("Enter"))
-print(num1 + num2)
+
+import shutil
+
+def copy_file(source_path, destination_path):
+    try:
+        shutil.copy(source_path, destination_path)
+        print(f"File '{source_path}' copied to '{destination_path}' successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Example usage:
+source_path = r"C:\Users\David\Downloads\55.jpg"  # Replace with your source file path
+destination_path = r'C:\Users\David\OneDrive\Desktop'  # Replace with your destination folder path
+
+copy_file(source_path, destination_path)
